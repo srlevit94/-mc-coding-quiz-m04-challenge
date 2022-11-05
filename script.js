762c176e9c7ee9f22e578b3f85d8a3124e04379e
shanelevites <srlevit94@gmail.com>
2022-11-05 13:51:55 -0700

    added elements for the questions and timer

--- script.js
+++ script.js
@@ -1,3 +1,68 @@
+var start = document.getElementById("start");
+var quiz = document.getElementById("quiz");
+var question = document.getElementById("question");
+var timer = document.getElementById("timer");
+
+var choiceA = document.getElementById("A");
+var choiceB = document.getElementById("B");
+var choiceC = document.getElementById("C");
+var choiceD = document.getElementById("D");
+
+var finalScore = document.getElementById("finalScore");
+
+var questions = [
+    {
+        question: "Question #1",
+        choiceA: "AnsA",
+        choiceB: "AnsB",
+        choiceC: "AnsC",
+        choiceD: "AnsD",
+        correct: "C"
+    },
+    {
+        question: "Question #2",
+        choiceA: "AnsA",
+        choiceB: "AnsB",
+        choiceC: "AnsC",
+        choiceD: "AnsD",
+        correct: "C"
+    },
+    {
+        question: "Question #3",
+        choiceA: "AnsA",
+        choiceB: "AnsB",
+        choiceC: "AnsC",
+        choiceD: "AnsD",
+        correct: "C"
+    },
+    {
+        question: "Question #4",
+        choiceA: "AnsA",
+        choiceB: "AnsB",
+        choiceC: "AnsC",
+        choiceD: "AnsD",
+        correct: "C"
+    },
+    {
+        question: "Question #5",
+        choiceA: "AnsA",
+        choiceB: "AnsB",
+        choiceC: "AnsC",
+        choiceD: "AnsD",
+        correct: "C"
+    },
+    {
+        question: "Question #6",
+        choiceA: "AnsA",
+        choiceB: "AnsB",
+        choiceC: "AnsC",
+        choiceD: "AnsD",
+        correct: "C"
+    },
+]
+
+
+
 var timeEl = document.querySelector("timer");
 var secondsLeft = 60;
 
