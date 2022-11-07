60ecefe2557447cd0e6499ad5b306929e5677764
shanelevites <srlevit94@gmail.com>
2022-11-06 18:18:02 -0800

    4/5 acceptance criteria met. need to save score w/ intials and add styling

--- README.md
+++ README.md
@@ -8,16 +8,16 @@
 ## Acceptance Criteria
 - GIVEN I am taking a code quiz
 - WHEN I click the start button**
-- THEN a timer** starts and I am presented with a question**
+- THEN a timer starts and I am presented with a question - DONE
 
 - WHEN I answer a question
-- THEN I am presented with another question
+- THEN I am presented with another question - DONE
 
 - WHEN I answer a question incorrectly
-- THEN time is subtracted from the clock
+- THEN time is subtracted from the clock - DONE
 
 - WHEN all questions are answered or the timer reaches 0
-- THEN the game is over
+- THEN the game is over - DONE
 
 - WHEN the game is over
 - THEN I can save my initials and score
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// defines variables using query selectors
 var startEl = document.getElementById("start");
 var quizEl = document.getElementById("quiz");
 var timerEl = document.getElementById("timer");
@@ -6,106 +7,123 @@ var choicesEl = document.getElementById("choices");
 var resultEl = document.getElementById("result");
 var finalScoreEl = document.getElementById("final-score");
 var initialsEl = document.getElementById("initials");
+var buttonA = document.getElementById("A");
+var buttonB = document.getElementById("B");
+var buttonC = document.getElementById("C");
+var buttonD = document.getElementById("D");
 
+//hides quiz and scoreboard until needed
 document.getElementById("quiz").style.display="none";
 document.getElementById("scoreboard").style.display="none";
 
-
+//An array of 5 questions, the possible options, and correct answer
 var questions = [
     {
         question: "1: The right answer is C",
-        choiceA: "AnsA",
-        choiceB: "AnsB",
-        choiceC: "AnsC",
-        choiceD: "AnsD",
-        correct: "C"
+        choiceA: "A",
+        choiceB: "B",
+        choiceC: "C",
+        choiceD: "D",
+        correctAns: "C"
     },
     {
         question: "2: The right answer is A",
-        choiceA: "AnsA",
-        choiceB: "AnsB",
-        choiceC: "AnsC",
-        choiceD: "AnsD",
-        correct: "A"
+        choiceA: "A",
+        choiceB: "B",
+        choiceC: "C",
+        choiceD: "D",
+        correctAns: "A"
     },
     {
         question: "3: The right answer is B",
-        choiceA: "AnsA",
-        choiceB: "AnsB",
-        choiceC: "AnsC",
-        choiceD: "AnsD",
-        correct: "B"
+        choiceA: "A",
+        choiceB: "B",
+        choiceC: "C",
+        choiceD: "D",
+        correctAns: "B"
     },
     {
         question: "4: The right answer is D",
-        choiceA: "AnsA",
-        choiceB: "AnsB",
-        choiceC: "AnsC",
-        choiceD: "AnsD",
-        correct: "D"
+        choiceA: "A",
+        choiceB: "B",
+        choiceC: "C",
+        choiceD: "D",
+        correctAns: "D"
     },
     {
         question: "5: The right answer is B",
-        choiceA: "AnsA",
-        choiceB: "AnsB",
-        choiceC: "AnsC",
-        choiceD: "AnsD",
-        correct: "B"
+        choiceA: "A",
+        choiceB: "B",
+        choiceC: "C",
+        choiceD: "D",
+        correctAns: "B"
     },
     
 ]
 
+//global varibales needed for timer, scorekeeping, and cycling through questions
 var finalQ = questions.length
 var currentQ= 0;
-var setTimer = 60;
+var secondsLeft = 10;
 var timerInterval;
 var score = 0;
 var correct;
 
 
 function cycleQuestions(){
-    if (currentQ === QuestionIndex){
-        return showScore();
+    if (currentQ === finalQ){
+        return showScoreBoard();
     } 
-    var currentQ = questions[QuestionIndex];
-    questionsEl.innerHTML = "<p>" + currentQ.question + "</p>";
-    buttonA.innerHTML = currentQ.A;
-    buttonB.innerHTML = currentQ.B;
-    buttonC.innerHTML = currentQ.C;
-    buttonD.innerHTML = currentQ.D;
+    var showCurrentQ = questions[currentQ];
+    questionEl.innerHTML = "<p>" + showCurrentQ.question + "</p>";
+    buttonA.innerHTML = showCurrentQ.choiceA;
+    buttonB.innerHTML = showCurrentQ.choiceB;
+    buttonC.innerHTML = showCurrentQ.choiceC;
+    buttonD.innerHTML = showCurrentQ.choiceD;
 };
 
 // Shows hidden quiz, starts timer
 function startQuiz(){
-    quiz.style.display = "block";
-    generateQuizQuestion();
+    document.getElementById("quiz").style.display="block";
+    cycleQuestions();
+    
 
     //Timer
     timerInterval = setInterval(function() {
-        setTimer--;
-        timer.textContent = "Time left: " + setTimer;
+        secondsLeft--;
+        timer.textContent = "Seconds Left: " + secondsLeft;
 
-        if(setTimer === 0) {
+        if(secondsLeft === 0) {
           clearInterval(timerInterval);
-          showScore();
+          showScoreBoard();
         }
       }, 1000);
 }
 
 // Checks whether answer is correct and adjusts timer/score accordingly
 function checkAnswer(answer){
-    correct = questions[QuestionIndex].correctAnswer;
+    correct = questions[currentQ].correctAns;
 
-    if (answer === correct && currentQuestionIndex !== finalQuestionIndex){
+    if (answer === correct && currentQ !== finalQ){
         score++;
-        QuestionIndex++;
-        generateQuizQuestion();
-    }else if (answer !== correct && QuestionIndex !== finalQuestionIndex){
-        QuestionIndex++;
+        currentQ++;
+        cycleQuestions();
+    }else if (answer !== correct && currentQ !== finalQ){
+        currentQ++;
         secondsLeft = secondsLeft - 5;
-        generateQuizQuestion();
+        cycleQuestions();
     }else{
-        showScore();
+        showScoreBoard();
     }
 }
-startEl.addEventListener("click",start);
+
+function showScoreBoard(){
+    startEl.style.display= "none"
+    quiz.style.display = "none"
+    document.getElementById("scoreboard").style.display="block";
+    clearInterval(timerInterval);
+    initialsEl.value = "";
+    finalScoreEl.innerHTML = "You got " + score + " out of " + questions.length + " correct!";
+}
+
+startEl.addEventListener("click", startQuiz);
